793660f253cfd3427d8008860a681715fb082f39
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-07-05 15:51:12 +0600

    React Date Range Added

--- client/src/Pages/Dashboard/AddRoom.jsx
+++ client/src/Pages/Dashboard/AddRoom.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import AddRoomForm from "../../components/Forms/AddRoomForm";
+import { imageUpload } from "../../API/utils";
+import useAuth from "../../components/Hooks/useAuth";
 
 const AddRoom = () => {
+	const { user } = useAuth();
 	const [loading, setLoading] = useState(false);
 	const [uploadButtonText, setUploadButtonText] = useState("Upload Image");
 
@@ -11,8 +14,8 @@ const AddRoom = () => {
 		const form = e.target;
 		const location = form.location.value;
 		const title = form.title.value;
-		const from = dates.startDate;
-		const to = dates.endDate;
+		// const from = dates.startDate;
+		// const to = dates.endDate;
 		const price = form.price.value;
 		const total_guest = form.total_guest.value;
 		const bedrooms = form.bedrooms.value;
@@ -20,6 +23,34 @@ const AddRoom = () => {
 		const description = form.description.value;
 		const category = form.category.value;
 		const image = form.image.files[0];
+
+		// Upload image
+		imageUpload(image)
+			.then((data) => {
+				const roomData = {
+					location: location,
+					title,
+					from,
+					to,
+					price: parseFloat(price),
+					total_guest,
+					bedrooms,
+					bathrooms,
+					image: data.data.display_url,
+					host: {
+						name: user?.displayName,
+						image: user?.photoURL,
+						email: user?.email,
+					},
+					category,
+				};
+				setLoading(false);
+			})
+			.catch((err) => {
+				console.log(err.message);
+				setLoading(false);
+			});
+
 		console.log(location);
 	};
 
